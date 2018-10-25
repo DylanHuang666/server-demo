5757aa21f276edcf924988d2b8fb7a5e6b9471b9
Dylan <huanglinjay123@outlook.com>
2018-10-25 21:29:46 +0800

    返回一个Promise对象，也可以像jQuery.ajax一样.then(请求成功要执行的函数,请求失败要执行的函数)传参

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-window.jQuery.ajax=function({url,method,body,success, fail}){
-    let request=new XMLHttpRequest()
-    request.onreadystatechange=()=>{
-        if(request.readyState==4){
-            if(request.status>=200 && request.status<300){
-                success.call(undefined,request.responseText)
-            }else if(request.status>=400){
-                fail.call(undefined,request)
+window.jQuery.ajax=function({url,method,body}){
+    return new Promise(function(resolve,reject){
+       let request=new XMLHttpRequest()
+        request.onreadystatechange=()=>{
+            if(request.readyState==4){
+                if(request.status>=200 && request.status<300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status>=400){
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
-    request.open(method,url)
-    request.send(body)
+        request.open(method,url)
+        request.send(body) 
+    })    
 }
 
 
 
 myButton.addEventListener('click',(e)=>{
     window.jQuery.ajax({
-        url:'/xxx',
+        url:'/xx',
         method:'GET',
-        success:(x)=>{
-            console.log(x)
-        },
-        fail:(x)=>{
-            console.log(x)
-        }
-    })
+    }).then(
+        (x)=>{console.log(x)},
+        (x)=>{console.log(x)})
 })
